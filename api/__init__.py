84f55cd4c5f5b5d65d2280a26cf808007b23bfa6
karython <karython.unai@gmail.com>
2025-09-24 20:55:41 -0300

    aula02 - 24/09 instalando bibliotecas e iniciando-as

--- api/__init__.py
+++ api/__init__.py
@@ -1,2 +1,21 @@
-# configuração da API
-# todas as instacias das bibliotecas são feitas aqui
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# instanciando o Flask e a Api
+app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+ma = Marshmallow(app)
+api = Api(app)
+
+
+
+#TODO: Apontar os modelos (tabelas)
+#      Apontar as Views
